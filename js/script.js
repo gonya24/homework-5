0abc2bda4068f14d82d49481597380f1d36bb72e
Illia <illia.gonya@icloud.com>
2023-04-02 20:57:56 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var x1 = prompt ('Enter first number') // Введение первого числа
+var x2 = prompt ('Enter second number') // Введение второго числа
+var x3 = prompt ('Enter third number') // Введение третьего числа 
+alert ((Number(x1) + Number(x2) + Number(x3)) / 3 )
